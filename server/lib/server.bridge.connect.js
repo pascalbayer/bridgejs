6a4ff871909fbe8745ca6b5682878b21ec47bf8c
s.wahn <sebastian.wahn@cubesoft.org>
2014-03-18 08:46:51 +0100

    add some initialization and empty functions to server.connect

--- server/lib/server.bridge.connect.js
+++ server/lib/server.bridge.connect.js
@@ -7,6 +7,18 @@
  * https://github.com/pascalbayer/bridgejs/blob/master/LICENSE
  */
 
+/**
+ * Connect provides an abstraction for basic connecting and communication features.
+ *
+ * @class Bridge.Connect
+ */
 Bridge.Connect = (function(){
+    var Connect = {};
+    var connectors = ['socket.io', ''];
 
-}())
+    Connect.connect = function(){};
+    Connect.send = function(){};
+    Connect.broadcast = function(){};
+
+    return Connect;
+}());
