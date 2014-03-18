201167a86bf580a8c0f0ce21fdc8a605c3722ff1
s.wahn <sebastian.wahn@cubesoft.org>
2014-03-18 08:45:40 +0100

    add connect class which should do the wrapping around socket.io or other communication frameworks

---
+++ server/lib/server.bridge.connect.js
@@ -0,0 +1,12 @@
+/*
+ * bridgejs
+ * http://bridgejs.com/
+ *
+ * Copyright (c) 2014 Pascal Bayer, Sebastian Wahn
+ * Licensed under the MIT license.
+ * https://github.com/pascalbayer/bridgejs/blob/master/LICENSE
+ */
+
+Bridge.Connect = (function(){
+
+}())
